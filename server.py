5ce4966eee8226ad8ab44aba61b609b4c311ca49
rachid <rachid@devru.sh>
2023-03-17 15:56:37 +0100

    define new functions

--- server.py
+++ server.py
@@ -5,12 +5,32 @@ import random
 import time
 
 
+def get_temperature():
+    # returns the current temperature
+    return random.randint(30, 45)
+
+
+def get_door():
+    # returns false if door is closed
+    return random.choice([True, False])
+
+
+def get_vent():
+    # return false if vent is off
+    return random.choice([True, False])
+
+
+def get_lum():
+    # return the current brightness
+    return random.randint(0, 100)
+
+
 async def send_info(websocket, path):
     # Envoyer des informations en WebSocket lorsque la connexion est établie
     while True:
 
         info = {
-            "temp": random.randint(30, 45), "gate": random.choice([True, False]), "vent": random.choice([True, False]), "lum": random.randint(0, 100)
+            "temperature": get_temperature(), "porte ouverte": get_door(), "ventilateur allume": get_vent(), "luminosite": get_lum()
         }
         info_json = json.dumps(info)
         await websocket.send(info_json)
