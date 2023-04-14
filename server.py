76d225bad4d36f11c3659ded519ae1dbf747d6c7
rachid <rachid@devru.sh>
2023-04-14 15:37:52 +0200

    create laserbox class

--- server.py
+++ server.py
@@ -3,26 +3,68 @@ import json
 import websockets
 import random
 import time
+import time
+import board
+import busio
+import adafruit_si7021
+import RPi.GPIO as GPIO
 
 
-def get_temperature():
-    # returns the current temperature
-    return random.randint(30, 45)
+class laserbox:
 
+    DOOR_PIN = 18
+    FAN_PIN = 23
+    LIGHT_PIN = 24
 
-def get_door():
-    # returns false if door is closed
-    return random.choice([True, False])
+    def read_temperature():
+        # returns the current temperature
+        i2c = busio.I2C(board.SCL, board.SDA)
+        temperature = False
+        while temperature == False:
+            try:
+                sensor = adafruit_si7021.SI7021(i2c)
+                temperature = sensor.temperature
+            except:
+                pass
+        # print(sensor.relative_humidity)
+        return temperature
 
+    def read_door():
+        # returns 1 if door is close
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(DOOR_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        val = GPIO.input(DOOR_PIN)
+        if val == 0:
+            return False
+        else:
+            return True
 
-def get_vent():
-    # return false if vent is off
-    return random.choice([True, False])
+    def control_fan(state):
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(FAN_PIN, GPIO.OUT)
+        if state is not True:
+            GPIO.output(FAN_PIN, GPIO.LOW)
+        else:
+            GPIO.output(FAN_PIN, GPIO.HIGH)
+        return state
 
+    def update_light(door_state):
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(LIGHT_PIN, GPIO.OUT)
+        if door_state is not True:
+            GPIO.output(LIGHT_PIN, GPIO.LOW)
+        else:
+            GPIO.output(LIGHT_PIN, GPIO.HIGH)
 
-def get_lum():
-    # return the current brightness
-    return random.randint(0, 100)
+    def read_vent():
+        # return false if vent is off
+        return random.choice([True, False])
+
+    def read_lum():
+        # return the current brightness
+        return random.randint(0, 100)
 
 
 async def send_info(websocket, path):
@@ -30,7 +72,7 @@ async def send_info(websocket, path):
     while True:
 
         info = {
-            "temperature": get_temperature(), "porte ouverte": get_door(), "ventilateur allume": get_vent(), "luminosite": get_lum()
+            "temperature": laserbox.read_temperature(), "porte ouverte": laserbox.read_door(), "ventilateur allume": laserbox.read_vent(), "luminosite": laserbox.read_lum()
         }
         info_json = json.dumps(info)
         await websocket.send(info_json)
